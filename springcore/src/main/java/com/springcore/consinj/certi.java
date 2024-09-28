1f9640aec5e031b6369014d5cfd07e73dbf3439f
siddharthk004 <siddharthkardile5@gmail.com>
2024-09-28 21:11:47 +0530

    updated codes / 28 sep / 9:11pm

---
+++ springcore/src/main/java/com/springcore/consinj/certi.java
@@ -0,0 +1,16 @@
+package com.springcore.consinj;
+
+public class certi {
+	String name;
+
+	public certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
